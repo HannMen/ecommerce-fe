ef64886bcaa1a7d6620fb06fa0482c7286fc1bd3
HannMen <hannermendozasanchez@gmail.com>
2023-07-29 16:29:57 -0400

    1st  commit

---
+++ src/services/authServices.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const URL_ROOT = "https://ecommerce-gj9t.onrender.com/api/auth";
+// const URL_ROOT = "http://localhost:4000/api/auth";
+
+const loginService = async (form) => {
+  const resp = await axios.post(`${URL_ROOT}/login`, form);
+  return resp;
+};
+
+const registerService = async (form) => {
+  const resp = await axios.post(`${URL_ROOT}/register`, form);
+  return resp;
+};
+
+const renewTokenService = async () => {
+  const resp = await axios.get(`${URL_ROOT}/renew`, {
+    headers: {
+      "x-token": localStorage.getItem("token"),
+    },
+  });
+  return resp;
+};
+
+export { loginService, registerService, renewTokenService };
---
+++ src/services/productServices.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const URL_ROOT = `https://ecommerce-gj9t.onrender.com/api/productos`;
+
+const getProductsService = async () => {
+  const response = await axios.get(`${URL_ROOT}`);
+  return response;
+};
+
+const getProductService = async (id) => {
+  const response = await axios.get(`${URL_ROOT}/${id}`);
+  return response;
+};
+
+export { getProductsService, getProductService };
