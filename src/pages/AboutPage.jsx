a9799b4d79c84c01fd75aebbe839b7f7afffca26
HannMen <hannermendozasanchez@gmail.com>
2023-08-03 23:03:07 -0400

    21th commit

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,6 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../styles/homepage.css";
 
 const AboutPage = () => {
   return (
@@ -10,29 +12,7 @@ const AboutPage = () => {
         </article>
       </main>
 
-      <footer className="bg-light py-3">
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-md-6">
-              <p className="text-center text-md-start">
-                &copy;2023 Elephant Institute. Todos los derechos reservados.
-              </p>
-            </div>
-            <div className="col-12 col-md-6">
-              <ul className="list-inline text-center text-md-end">
-                <li className="list-inline-item">
-                  <a
-                    href="https://www.facebook.com/elephantinstituteCol/"
-                    target="_blank"
-                  >
-                    <i className="bi bi-facebook"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 };
