d5bd1f6f4076653e3e08a71eae014db1e1db58a9
HannMen <hannermendozasanchez@gmail.com>
2023-08-03 22:51:37 -0400

    20th commit

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -86,6 +86,29 @@ const CartPage = () => {
           )}
         </article>
       </section>
+      <footer className="bg-light py-3">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <p className="text-center text-md-start">
+                &copy;2023 Elephant Institute. Todos los derechos reservados.
+              </p>
+            </div>
+            <div className="col-12 col-md-6">
+              <ul className="list-inline text-center text-md-end">
+                <li className="list-inline-item">
+                  <a
+                    href="https://www.facebook.com/elephantinstituteCol/"
+                    target="_blank"
+                  >
+                    <i className="bi bi-facebook"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -71,6 +71,29 @@ const LoginPage = () => {
           <pre>{JSON.stringify(user, null, 2)}</pre>
         </article>
       </section>
+      <footer className="bg-light py-3">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <p className="text-center text-md-start">
+                &copy;2023 Elephant Institute. Todos los derechos reservados.
+              </p>
+            </div>
+            <div className="col-12 col-md-6">
+              <ul className="list-inline text-center text-md-end">
+                <li className="list-inline-item">
+                  <a
+                    href="https://www.facebook.com/elephantinstituteCol/"
+                    target="_blank"
+                  >
+                    <i className="bi bi-facebook"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
